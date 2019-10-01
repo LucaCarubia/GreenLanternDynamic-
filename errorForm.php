a1c2c4cd61d9f9c3900b92271f9d0a2c9a0a2f97
Luca Carubia <luca.carubia42@gmail.com>
2019-10-01 10:37:11 +0200

    ajout de la methode filter_var

--- errorForm.php
+++ errorForm.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<p>Erreur lors de l'envoi du formulaire, veuillez remplir tout les champs correspondant.</p>";
+echo "<p>Erreur lors de l'envoi du formulaire, veuillez remplir tout les champs correspondant ou vérifier que votre email est correcte .</p>";
 
 header( "refresh:5;url=joinUs.php" );
 
--- formProcessing.php
+++ formProcessing.php
@@ -1,7 +1,7 @@
 <?php
 
-if (!empty($_POST['firstName']) && (!empty($_POST['lastName'])) && (!empty($_POST['email']))
-    && (!empty($_POST['choice']) && (!empty($_POST['message'])))) {
+if (!empty($_POST['firstName']) && (!empty($_POST['lastName'])) && (!empty($_POST['email'])
+        && (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))) && (!empty($_POST['choice']) && (!empty($_POST['message'])))) {
     header('Location: successForm.php');
 } else {
     header('Location: errorForm.php');
